88c893260081b40714fe8324d8ea0cccc9c66226
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-09-04 18:30:06 -0300

    todos py

---
+++ hola.py
@@ -0,0 +1,17 @@
+hola="adrian"
+
+print(hola) 
+## esto es un comentario
+
+
+def mensaje():  ##declaracion de la funcion
+    print("esto es una prueba de fnc")  ## cuerpo de la funcion
+mensaje()  ## llamada de la funcion
+
+
+def suma(n1,n2):
+    print (n1 + n2)
+
+suma(5,7)
+suma(10,20)
+
---
+++ suma2.py
@@ -0,0 +1,20 @@
+
+#a=int(input ('ingrese el 1st numero: '))
+#b=int(input ('ingrese el 2nd numero: '))
+
+#def suma(a,b):
+#   return(a+b)
+
+#print(f'la suma de los numeros es: '+ suma)
+
+
+
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+def sum(a, b):
+     return (a + b)
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+ 
