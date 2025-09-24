790ff30bccd504aa4cf5bc5917856ef9d91a23ad
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-09-24 08:21:13 -0300

    btn send form

--- grafica/entry_ejercicio.py
+++ grafica/entry_ejercicio.py
@@ -20,6 +20,14 @@ cuadroPass=Entry(miFrame)
 cuadroPass.grid(row=3,column=1, padx=10, pady=10)
 cuadroPass.config(show="*")  #muestra asteriscos por seguridad
 
+areaTexto=Text(miFrame, width=15, height=5)
+areaTexto.grid(row=3, column=1, padx=10, pady=10)
+areaTexto.config(fg="red", bg="gray",)
+
+scrollv=Scrollbar(miFrame, command=areaTexto.yview)
+scrollv.grid(row=3, column=2, sticky="nsew")
+areaTexto.config(yscrollcommand=scrollv.set)
+
 #======LABEL======
 
 nombreLabel=Label(miFrame, text="Nombre: ")
@@ -33,5 +41,14 @@ edadLabel.grid(row=2,column=0, sticky="w", padx=10, pady=10)
 
 passLabel=Label(miFrame, text="Password: ")
 passLabel.grid(row=3,column=0, sticky="w", padx=10, pady=10)
+
+textoLabel=Label(miFrame, text="Mensaje")
+textoLabel.grid(row=4, column=0, padx=10, pady=10)
+
+def enviar():
+    print("se envio el mensaje")
+
+enviarbtn=Button(root, text="enviar mensaje", command=enviar)
+enviarbtn.pack()
 
 root.mainloop()
