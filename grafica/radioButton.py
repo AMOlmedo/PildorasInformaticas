7fd1ef5d13abebc7d17464a98bc5905e9464c80a
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-10-02 07:40:39 -0300

    modify radioButton.py

--- grafica/radioButton.py
+++ grafica/radioButton.py
@@ -1,11 +1,25 @@
+# RadioButtom seleccion excluyente, solo 1  opcion a seleccionar
+
 from tkinter import *
 root=Tk()  
 
 opcion=IntVar()   #fnc q define en Entry es un int
 
-Radiobutton(root, text="Masculino", variable=opcion, value=(1)).pack()
-Radiobutton(root, text="Femenino", variable=opcion, value=(2)).pack()
+Label(root, text="Genero").pack
+def imprimir():
+    if opcion.get() ==1:
+        mensaje.config(text="Has seleccionado Masculino")
+    elif opcion.get()==2:
+        mensaje.config(text="Has seleccionado Femenino")
+    else:
+        mensaje.config(text="Has seleccionado OTROS")
 
+Radiobutton(root, text="Masculino", variable=opcion, value=(1), command=imprimir).pack()
+Radiobutton(root, text="Femenino", variable=opcion, value=(2), command=imprimir).pack()
+Radiobutton(root, text="Otras", variable=opcion, value=(3), command=imprimir).pack()
+
+mensaje=Label(root)
+mensaje.pack()
 
 root.mainloop()
 
