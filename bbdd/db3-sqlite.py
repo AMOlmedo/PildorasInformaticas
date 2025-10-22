b61a670c0854f740dc19b10bfc1a224ab045a352
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-10-22 09:17:53 -0300

    upgrade db pk incremental

--- bbdd/db3-sqlite.py
+++ bbdd/db3-sqlite.py
@@ -2,27 +2,29 @@ import sqlite3
 import os
 
 directorio_script = os.path.dirname(os.path.abspath(__file__))
-ruta_bd = os.path.join(directorio_script, "gestionPersonas.db")
+ruta_bd = os.path.join(directorio_script, "gestionPersMascotas.db")
 
 miConexion=sqlite3.connect(ruta_bd)
 miCursor=miConexion.cursor()
 
-miCursor.execute('''
-    CREATE TABLE personajes (
-                 CODIGO INTEGER PRIMARY KEY,
+miCursor.execute('''    
+    CREATE TABLE miembros (
+                 ID INTEGER PRIMARY KEY AUTOINCREMENT,
                  NOMBRE VARCHAR(30),
                  EDAD INTEGER,
                  PAIS VARCHAR(50))
                  ''')
-
+# la triple  ''' es para poder escribir en varias lineas las sentencia de sql
 personajesLista=[
-        (1, "adrian", 52, "arg"),
-        (2, "marti", 16, "arg"),
-        (3, "kuka", 10, "bol"),
-        (4, "rita", 16, "usa")
+        ("adrian", 52, "arg"),
+        ("marti", 16, "arg"),
+        ("kuka", 10, "bol"),
+        ("rita", 16, "usa"),
+        ("miti", 10, "chi")
+
 ]
     
-miCursor.executemany("INSERT INTO personajes VALUES(?,?,?,?)", personajesLista)
+miCursor.executemany("INSERT INTO miembros VALUES(?,?,?)", personajesLista)
 
 miConexion.commit()
 miConexion.close()
