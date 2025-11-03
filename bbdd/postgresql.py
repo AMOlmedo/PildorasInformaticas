f11e7ff4c24d7343fa53f1f1311529f46f6d0da3
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-11-03 08:37:13 -0300

    update postgresql.py

--- bbdd/postgresql.py
+++ bbdd/postgresql.py
@@ -3,11 +3,14 @@ import psycopg2
 try:
     conexion_db=psycopg2.connect(
         host='localhost',
+        port=5432, 
+        database="clientes_db",
         user='postgres',
-        ##password='',
+        password='12345',
         database='prueba'
     )
     print("conexion exitosa")
+    
 except Exception as ex:
     print(ex)
 finally:
